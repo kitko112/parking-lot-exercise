cdbb36e6b0b826eeb7ac545394aff47c9c0a5845
Kit Ko <kitko112@gmail.com>
2023-01-23 22:56:45 +0000

    Implement insert ticket on ticket repository

---
+++ src/repository/ticketRepository.ts
@@ -0,0 +1,20 @@
+import type { ParkingTicket } from '../model/parkingTicket';
+import type { ITicketRepository } from './ITicketRepository';
+
+export class TicketRepository implements ITicketRepository {
+
+    private _ticketMap = new Map<string, ParkingTicket>();
+    private _ticketCounter: number = 1
+
+    public insertTicket(ticket: ParkingTicket): ParkingTicket {
+        const ticketNumber = (this._ticketCounter++)
+            .toString()
+            .padStart(3, '0');
+        const insertedTicket: ParkingTicket = {
+            ...ticket,
+            ticketNumber,
+        }
+        this._ticketMap.set(ticketNumber, insertedTicket);
+        return insertedTicket;
+    }
+}
