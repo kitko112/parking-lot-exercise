997c96943181016f2ffc5775cd2ea2961b843cc4
Kit Ko <kitko112@gmail.com>
2023-01-23 23:37:09 +0000

    Add get ticket method to ticket service interface

--- src/service/ITicketService.ts
+++ src/service/ITicketService.ts
@@ -2,4 +2,5 @@ import type { ParkingTicket } from '../model/parkingTicket';
 
 export interface ITicketService {
     createTicket(spotNumber: number): ParkingTicket;
+    getTicket(ticketNumber: string): ParkingTicket;
 }
