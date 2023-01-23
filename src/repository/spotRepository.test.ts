2ec495419efbc22c5280b3016d9158feef7eff2b
Kit Ko <kitko112@gmail.com>
2023-01-23 16:47:28 +0000

    Add test case of no available small spot

--- src/repository/spotRepository.test.ts
+++ src/repository/spotRepository.test.ts
@@ -1,4 +1,5 @@
 import type { ParkingSpot } from '../model/parkingSpot';
+import { buildSpotFn, SpotRepository } from './spotRepository';
 
 describe('Spot Repository test suite', () => {
     describe('Get available small spot method', () => {
@@ -8,10 +9,19 @@ describe('Spot Repository test suite', () => {
             spotSize: 'small'
         };
         it('should be return the first available small spot when there are more than one', () => {
-            const repository = new SpotRepository();
+            const initSmallSpots = buildSpotFn(1, 'small', false, 10);
+            const repository = new SpotRepository(initSmallSpots);
             const smallSpot = repository.getAvailableSmallSpot();
 
             expect(smallSpot).toEqual(emptySmallSpot);
+        });
+
+        it('should be return undefined when there is none of available small spot', () => {
+            const initSmallSpots = buildSpotFn(1, 'small', true, 10);
+            const repository = new SpotRepository(initSmallSpots);
+            const smallSpot = repository.getAvailableSmallSpot();
+
+            expect(smallSpot).toBeUndefined();
         });
     })
 })
