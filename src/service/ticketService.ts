59fd8c58077a3ead4f13efdee0764fc7b0043307
Kit Ko <kitko112@gmail.com>
2023-01-23 22:51:35 +0000

    Implement create ticket method on ticket service

---
+++ src/service/ticketService.ts
@@ -0,0 +1,23 @@
+import type { ParkingTicket } from '../model/parkingTicket';
+import type { ITicketRepository } from '../repository/ITicketRepository';
+import type { ITicketService } from './ITicketService';
+
+export class TicketService implements ITicketService {
+
+    private _ticketRepository: ITicketRepository;
+
+    constructor(
+        ticketRepository: ITicketRepository
+    ) {
+        this._ticketRepository = ticketRepository;
+    }
+
+    public createTicket(spotNumber: number): ParkingTicket {
+        const newTicket: ParkingTicket = {
+            spotNumber: spotNumber,
+            entryDateTime: new Date(),
+        }
+        const insertedTicket = this._ticketRepository.insertTicket(newTicket);
+        return insertedTicket;
+    }
+}
