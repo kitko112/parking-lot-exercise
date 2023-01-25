e51c680354104ccb65650ac2527639f49645a5dd
Kit Ko <kitko112@gmail.com>
2023-01-25 22:49:15 +0000

    Remove unrequired logic on diff date time function

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,5 @@
 export const diffDateTimeToHours = (oldDate1: Date, newDate2: Date): number => {
     const diffMs = newDate2.getTime() - oldDate1.getTime();
     const diffHrs = diffMs / 3600000;
-    return Math.abs(diffHrs);
+    return diffHrs;
 }
